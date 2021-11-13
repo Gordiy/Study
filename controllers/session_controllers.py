2ac56518bb8100f1c057abfa5a7e63805e6ed860
hordii.rushynets <hordii.rushynets@globallogic.com>
2021-11-13 02:13:32 +0200

    Enh: read json file from other directory

--- controllers/session_controllers.py
+++ controllers/session_controllers.py
@@ -1,34 +1,36 @@
 import os
-from .file_controllers import write_json_file, read_json_file
+
+from .file_controllers import read_json_file, write_json_file
+
 
 def find_session_db(session_id):
-    path = f"{os.getcwd()}/user_session/sessions.json"
+    path = f"{os.getcwd()}/temp/sessions.json"
     data = read_json_file(path)
     session = None
-    if data and 'sessions' in data:
-        for i in range(len(data)): 
-            if session_id in data['sessions'][i]:
+    if data and "sessions" in data:
+        for i in range(len(data)):
+            if session_id in data["sessions"][i]:
                 session = data[i]
-    
+
     return session
-    
+
 
 def create_session_db(session):
-    path = f"{os.getcwd()}/user_session/sessions.json"
+    path = f"{os.getcwd()}/temp/sessions.json"
     data = read_json_file(path)
-    if data and ['sessions'] in data:
-        sessions = data['sessions'].append(session.session_obj())
+    if data and "sessions" in data:
+        data["sessions"].append(session.session_obj())
     else:
-        sessions = {'sessions': [session.session_obj()]}
-    write_json_file(path, sessions)
+        data = {"sessions": [session.session_obj()]}
+    write_json_file(path, data)
 
 
 def remove_session_db(session_id):
-    path = f"{os.getcwd()}/user_session/sessions.json"
+    path = f"{os.getcwd()}/temp/sessions.json"
     status = False
     sessions = read_json_file(path)
-    if sessions and ['sessions'] in sessions:
-        sessions = sessions['sessions']
+    if sessions and sessions in sessions:
+        sessions = sessions["sessions"]
         for i in range(sessions):
             if sessions[i][session_id]:
                 del sessions[i]
