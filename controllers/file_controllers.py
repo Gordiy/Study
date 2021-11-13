5be51e277bc44cf1bb01ee9b04326f6f50fccddf
hordii.rushynets <hordii.rushynets@globallogic.com>
2021-11-13 02:11:09 +0200

    Enh: refactor read from json file function

--- controllers/file_controllers.py
+++ controllers/file_controllers.py
@@ -2,11 +2,15 @@ import json
 
 
 def write_json_file(path, data):
-    with open(path, 'w', encoding='utf-8') as f:
+    with open(path, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
+        f.close()
 
 
 def read_json_file(path):
-    file = open(path, 'r')
-    data = json.loads(file.read())
-    return data
+    with open(path) as f:
+        data = json.load(f)
+        if data:
+            return data
+        else:
+            return {}
