faaa214f7b918156d7c16a18afcea45fe3a4305b
hordii.rushynets <hordii.rushynets@globallogic.com>
2021-11-13 02:15:43 +0200

    Add: add time expiration attribute to session object

--- controllers/session.py
+++ controllers/session.py
@@ -1,27 +1,38 @@
+import datetime
 import random
 import string
 
 
 class Session:
-    def __init__(self, data):
+    def __init__(self, user_info):
         self.__session_id = self.get_random_string(16)
-        self.__data = data
-
-    def session_obj(self):
-        return {
-            self.__session_id: {
-                "data": self.__data
-            }
-        }
+        self.__user_info = user_info
+        self.__time_exp = self.time_expiration()
 
     @property
     def session_id(self):
         return self.__session_id
 
+    @property
+    def time_exp(self):
+        return self.__time_exp
+
+    def session_obj(self):
+        return {
+            self.__session_id: {
+                "time_exp": str(self.__time_exp),
+                "user_info": self.__user_info,
+            }
+        }
+
+    def time_expiration(self, hours=1):
+        current_date_time = datetime.datetime.now()
+        future_date_time = current_date_time + datetime.timedelta(hours=hours)
+        return future_date_time
+
     @staticmethod
     def get_random_string(length):
         # choose from all lowercase letter
         letters = string.ascii_lowercase
-        result_str = ''.join(random.choice(letters) for i in range(length))
+        result_str = "".join(random.choice(letters) for i in range(length))
         return result_str
-    
