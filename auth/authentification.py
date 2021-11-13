4361e0191b243e6abdc7328c3d898b8a8a46a46c
hordii.rushynets <hordii.rushynets@globallogic.com>
2021-11-13 19:08:39 +0200

    Add: add api that login user

--- auth/authentification.py
+++ auth/authentification.py
@@ -3,7 +3,11 @@ from flask import Blueprint, Response, redirect, render_template, request
 from controllers.session import Session
 from controllers.session_controllers import create_session_db, find_session_db
 from controllers.user import User
-from controllers.user_controllers import create_user_db, find_user_db
+from controllers.user_controllers import (
+    create_user_db,
+    find_user_by_username_db,
+    find_user_db,
+)
 
 bp_auth = Blueprint("auth", __name__, url_prefix="/auth")
 
@@ -24,5 +28,29 @@ def register():
         resp.set_cookie("session-id", session.session_id, expires=session.time_exp)
 
         return resp
+    else:
+        return {"status": "fail", "message": "Not enough attributes."}
+
+
+@bp_auth.route("login/", methods=["POST"])
+def login():
+    if "username" in request.form and "password" in request.form:
+        user = find_user_by_username_db(request.form["username"])
+        if (
+            user
+            and user["username"] == request.form["username"]
+            and user["password"] == request.form["password"]
+        ):
+            # create user's session
+            session = Session(user.get_user())
+            create_session_db(session)
+
+            # return session id to client
+            resp = Response({"status": "success"})
+            resp.set_cookie("session-id", session.session_id, expires=session.time_exp)
+
+            return resp
+        else:
+            return {"status": "fail", "message": "Username or password incorrect."}
     else:
         return {"status": "fail", "message": "Not enough attributes."}
